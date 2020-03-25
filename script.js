5aacfb5b897884ef5ee656b37a139c34b2d8be77
scadler <scadler@s.sfusd.edu>
2020-03-25 01:27:10 +0000

    added randomized and tumbler buttons

--- script.js
+++ script.js
@@ -113,6 +113,18 @@ $(".cell").mousedown(function(){
             $(this).css('background-color', "rgb(255, 255, 255)")
         }
 });
+var clicked = false;
+$("#autoStep").click(function(){
+    clicked = !clicked
+    if(clicked === true){
+        $("#autoStep").css('background-color', "rgb(203, 203, 203)")
+        $("#autoStep").css('border', "1px solid rgb(173, 173, 173)")
+    }
+    else{
+        $("#autoStep").css('background-color', "rgb(255, 255, 255)")
+        $("#autoStep").css('border', "1px solid rgb(221, 221, 221)")
+    }
+});
 $("#clear").click(function(){
     clear()
 });
@@ -121,6 +133,7 @@ var LWSSCells = ["100","103","124","140","144","161","162","163","164"]
 var MWSSCells = ["101","102","103","104","105","120","125","145","160","164","182"]
 var MWSSCells = ["102","103","120","125","146","160","166","181","182","183","184","185","186"]
 var Pentadecathlon = ["186","191","204","205","207","208","209","210","212","213","226","231"]
+var Tumbler = ["84","85","89","90","104","106","108","110","124","126","128","130","146","148","165","166","168","169","185","186","188","189"]
 $("#glider").click(function(){
    drawCells(gliderCells)
 });
@@ -135,6 +148,19 @@ $("#HWSS").click(function(){
 });
 $("#Pentadecathlon").click(function(){
    drawCells(Pentadecathlon)
+});
+$("#Tumbler").click(function(){
+   drawCells(Tumbler)
+});
+$("#Randomize").click(function(){
+    var n = 0
+    randomArray = [""]
+    while(n < 201){
+        randomArray[n] = Math.floor(Math.random()*399)
+        n++
+        console.log(randomArray)
+    }
+    drawCells(randomArray)
 });
 function clear(){
     var cell = 0
